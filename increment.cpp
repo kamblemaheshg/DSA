5e39810a0984bac7413ac023d4b22d720fc65701
Mahesh <themaheshkamble@gmail.com>
2023-07-29 00:30:53 +0530

    Post incremtn

---
+++ increment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    // variable
+    int a=5;
+
+    cout<<"Value is : ";
+
+    // post increament 
+    cout << a++;
+    return 0;
+}
